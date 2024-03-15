dc2c9083aa8ee46b9e0d5212892386cfbbfee943
matthewcroughan <matt@croughan.sh>
2024-03-15 03:35:23 +0000

    wip

--- makeWindows11Image/default.nix
+++ makeWindows11Image/default.nix
@@ -2,6 +2,6 @@
 { ... }@args: (wfvm.lib.makeWindowsImage {
   windowsImage = (fetchtorrent {
     url = "https://archive.org/download/windows11_20220930/windows11_20220930_archive.torrent";
-    hash = "sha256-vym/dL3zwQDvPRmdvLSEeO/8toIEeCqfpyrmeEOnldo=";
+    hash = "sha256-jGHlnWmWN4iYd5o+cm0y3pCOwdkB/zSyiRdn+rIs4ic=";
   }) + "/Win11_22H2_English_x64v1.iso";
 } // args).overrideAttrs (_: { passthru.runScript = callPackage ./run.nix {}; })
