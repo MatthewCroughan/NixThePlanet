85e415003805de84d71142f4d352e0ddf2632c77
matthewcroughan <matt@croughan.sh>
2024-10-17 20:28:45 +0100

    use btih

--- makeWindows11Image/default.nix
+++ makeWindows11Image/default.nix
@@ -1,7 +1,7 @@
 { wfvm, fetchtorrent, callPackage }:
 { ... }@args: (wfvm.lib.makeWindowsImage {
   windowsImage = (fetchtorrent {
-    url = "https://archive.org/download/windows11_20220930/windows11_20220930_archive.torrent";
-    hash = "sha256-jGHlnWmWN4iYd5o+cm0y3pCOwdkB/zSyiRdn+rIs4ic=";
-  }) + "/Win11_22H2_English_x64v1.iso";
+    url = "magnet:?xt=urn:btih:89e7e3f95f11f962c1e47dc95ccc24c99f385a68&dn=win-10-21-h-1-english-x-64_20210711&tr=http%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=http%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&ws=http://ia601501.us.archive.org/10/items/&ws=https://archive.org/download/";
+    hash = "";
+  }) + "/Win10_21H1_English_x64.iso";
 } // args).overrideAttrs (_: { passthru.runScript = callPackage ./run.nix {}; })
